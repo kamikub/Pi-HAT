c8218b999a069f7ee7324b0115e347a497f48197
kamkub <kmlsal001@myuct.ac.za>
2021-06-11 19:28:41 +0000

    operating code for the uHat

---
+++ Software/Tuner.py
@@ -0,0 +1,19 @@
+
+#setup the pins
+	#user selects if they want the buzzer on or off
+
+##Thread 1
+#take in the ADC input sample
+
+#fft it
+
+#read txt file
+
+#compare input to the selected from textfile
+
+#provide output to user
+#repeat
+
+##Thread 2
+#read buttons input
+		#if pressed interrupt thread one and restart
